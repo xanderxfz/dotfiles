47f01827f2029dd21f964e68633792a7d7b0be8a
xanderxfz <xanderxfz.ua@gmail.com>
2024-03-26 21:47:24 +0200

    Fix apply.sh

--- apply.sh
+++ apply.sh
@@ -6,6 +6,7 @@ DOTFILES_PATH=$(cd $(dirname $0); pwd)
 
 for f in .??*; do
     [[ $f == ".git" ]] && continue
+    [[ $f == ".ssh" ]] && continue
     [[ $f == ".gitignore" ]] && continue
 
     ln -sfnv $DOTFILES_PATH/$f ~/$f
