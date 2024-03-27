ea3b526b338cd7cb7ee6e3e951bd2bebb53f224b
xanderxfz <xanderxfz.ua@gmail.com>
2024-03-27 16:40:13 +0200

    Update .zshrc

--- .zshrc
+++ .zshrc
@@ -2,7 +2,7 @@ export ZSH="$HOME/.oh-my-zsh"
 
 ZSH_THEME="gentoo"
 
-plugins=(git sudo docker kubectl helm terraform golang brew)
+plugins=(git sudo docker kubectl helm terraform golang brew zsh-autosuggestions)
 
 source $ZSH/oh-my-zsh.sh
 
--- apply.sh
+++ apply.sh
@@ -15,3 +15,13 @@ done
 # Install tmux tpm if it's missed
 TPM_PATH=~/.tmux/plugins/tpm
 [[ ! -d $TPM_PATH ]] && git clone https://github.com/tmux-plugins/tpm $TPM_PATH
+
+
+
+# Install oh-my-zsh
+OH_MY_ZSH_PATH=~/.oh-my-zsh
+[[ ! -d $OH_MY_ZSH_PATH ]] && sh -c "$(curl -fsSL https://install.ohmyz.sh/)"
+
+# Install zsh-autosuggestions
+ZSH_AUTOSUGGESTIONS=~/.oh-my-zsh/custom/plugins/zsh-autosuggestions
+[[ ! -d $ZSH_AUTOSUGGESTIONS ]] && git clone https://github.com/zsh-users/zsh-autosuggestions $ZSH_AUTOSUGGESTIONS && source ~/.zshrc
